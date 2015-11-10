f2e5ccc466f08fe1441bcdf47d19fe629730106c
kyzw <kota@192.168.42.144>
2015-11-10 15:30:30 +0900

    jQuery Validation Engineで入力チェック

--- layering/src/main/resources/static/js/validate.js
+++ layering/src/main/resources/static/js/validate.js
@@ -1,25 +1,4 @@
 $(function() {
-	$("form").submit(
-			function() {
-				if ($("input[name='username']").val() == "") {
-					if ($("span").css("color") != "red") {
-						$("input[name='username']").css("border", "1px solod red").before("<span>ユーザー名を入力してください</span>");
-						$("span").css("color", "red");
-					}
-					return false;
-				}
-			})
-			
-			$("form").submit(
-			function() {
-				if ($("input[name='password']").val() == "") {
-					if ($("span").css("color") != "red") {
-						$("input[name='password']").css("border", "1px solod red").before("<span>パスワードを入力してください</span>");
-						$("span").css("color", "red");
-					}
-					return false;
-				}
-			})
-		
+	jQuery("#form-login").validationEngine();
+	jQuery("#form-list").validationEngine();
 });
-
